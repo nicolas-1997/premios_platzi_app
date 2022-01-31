e311d91f821642b78d15b3e16e855dda8d3035bd
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2022-01-31 18:32:31 -0300

    creando_vistas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    #Ex: /polls/
+    path("", views.index, name="index"),
+    #Ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    #Ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    #Ex: /polls/5/votes/
+    path("<int:question_id>/votes/", views.votes, name="votes")
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estás en la pagina principals")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero: {question_id}")
+    
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero: {question_id}")
+    
+def votes(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero:  {question_id}")
+    
